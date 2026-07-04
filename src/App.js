ccd2ebb8d8202ba743c54c5dac94dfccc7e7f5d5
STEPHEN PROPHETICSON <lesasteven0@gmail.com>
2026-07-04 02:11:34 +0200

    a clean initial structure

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello
         </p>
         <a
           className="App-link"
